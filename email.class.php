859985493129a5a638565468108cde9bfbfb1597
Shane Shipston <shane@iconictouch.com>
2013-01-16 22:21:15 +0000

    Make email REGEXP more accessible

--- email.class.php
+++ email.class.php
@@ -19,6 +19,7 @@ class Email
 	private $uid;
 	private $headers = array();
 	private $attachments = array();
+	private $email_pattern = '/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/';
 	
 	/**
 	 * New Email
@@ -252,8 +253,7 @@ class Email
 	 */
 	private function validate_email($email) 
 	{
-		$pattern = '/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/';
-		if(preg_match($pattern, $email)) return true;
+		if(preg_match($this->email_pattern, $email)) return true;
 		
 		return false;
 	}
